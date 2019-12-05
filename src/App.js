37a9ab57233fe146cb2d58c34fac6870820586d2
Vivek Eswaran <vivek_eswaran@apple.com>
2019-12-04 23:14:50 -0600

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.App}>
+          <h2>Burger Builder</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
